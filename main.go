a3df1e53693e5f04a539bd69097b864a2523269e
Teh|Striker <kristianhanus@gmail.com>
2022-07-11 23:30:32 +0200

    Cookies handling

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/rs/cors"
 	"github.com/strikersk/user-auth/src/handlers"
 	"github.com/strikersk/user-auth/src/jwt"
-	"github.com/strikersk/user-auth/src/redis"
 	userRepository "github.com/strikersk/user-auth/src/repository"
 	userServices "github.com/strikersk/user-auth/src/service"
 	"net/http"
@@ -22,13 +21,11 @@ func main() {
 
 	userHandling := handlers.NewUserHandler(&userService)
 	jwtHandling := handlers.NewJwtHandler(&userService, jwtConfig)
-
-	redisRouter := myRouter.PathPrefix("/redis").Subrouter()
-	redisRouter.HandleFunc("/login", redis.Signin).Methods("POST")
-	redisRouter.HandleFunc("/welcome", redis.Welcome).Methods("GET")
+	cookiesHandling := handlers.NewCookiesHandler("session_token", &userService)
 
 	userHandling.EnrichRouter(myRouter)
 	jwtHandling.EnrichRouter(myRouter)
+	cookiesHandling.EnrichRouter(myRouter)
 
 	fmt.Println(http.ListenAndServe(":4000", cors.AllowAll().Handler(myRouter)))
 }
--- src/handlers/CookiesHandler.go
+++ src/handlers/CookiesHandler.go
@@ -1,68 +1,73 @@
-package redis
+package handlers
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	uuid "github.com/satori/go.uuid"
+	"github.com/gorilla/mux"
+	"github.com/strikersk/user-auth/src/domain"
+	"github.com/strikersk/user-auth/src/ports"
 	"net/http"
 	"time"
 )
 
-var users = map[string]string{
-	"user1": "password1",
-	"user2": "password2",
+type CookiesHandler struct {
+	TokenName string
+	Service   ports.IUserService
 }
 
-const tokenName = "session_token"
-
-// Create a struct that models the structure of a user, both in the request body, and in the DB
-type Credentials struct {
-	Password string `json:"password"`
-	Username string `json:"username"`
+func NewCookiesHandler(tokenName string, service ports.IUserService) CookiesHandler {
+	return CookiesHandler{
+		TokenName: tokenName,
+		Service:   service,
+	}
 }
 
-func Signin(w http.ResponseWriter, r *http.Request) {
-	var creds Credentials
+func (h CookiesHandler) EnrichRouter(router *mux.Router) {
+	jwtRouter := router.PathPrefix("/cookies").Subrouter()
+	jwtRouter.HandleFunc("/login", h.Login).Methods(http.MethodPost)
+	jwtRouter.HandleFunc("/welcome", h.Welcome).Methods(http.MethodGet)
+}
+
+func (h CookiesHandler) Login(w http.ResponseWriter, r *http.Request) {
+	var reqUser domain.Credentials
 	// Get the JSON body and decode into credentials
-	err := json.NewDecoder(r.Body).Decode(&creds)
+	err := json.NewDecoder(r.Body).Decode(&reqUser)
 	if err != nil {
 		// If the structure of the body is wrong, return an HTTP error
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	// Get the expected password from our in memory map
-	expectedPassword, ok := users[creds.Username]
+	persistedUser, err := h.Service.ReadUser(r.Context(), reqUser.Username)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 	// If a password exists for the given user
 	// AND, if it is the same as the password we received, the we can move ahead
 	// if NOT, then we return an "Unauthorized" status
-	if !ok || expectedPassword != creds.Password {
+	if persistedUser.Password != reqUser.Password {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 
 	// Create a new random session token
-	sessionToken := uuid.NewV4().String()
-
-	if err = Connection.CreateCache(sessionToken, creds.Username); err != nil {
-		// If there is an error in setting the cache, return an internal server error
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	sessionToken := base64.URLEncoding.EncodeToString([]byte(reqUser.Username))
 
 	// Finally, we set the client cookie for "session_token" as the session token we just generated
 	// we also set an expiry time of 120 seconds, the same as the cache
 	http.SetCookie(w, &http.Cookie{
-		Name:    tokenName,
+		Name:    h.TokenName,
 		Value:   sessionToken,
 		Expires: time.Now().Add(120 * time.Second),
 	})
 }
 
-func Welcome(w http.ResponseWriter, r *http.Request) {
+func (h CookiesHandler) Welcome(w http.ResponseWriter, r *http.Request) {
 	// We can obtain the session token from the requests cookies, which come with every request
-	c, err := r.Cookie(tokenName)
+	c, err := r.Cookie(h.TokenName)
 	if err != nil {
 		if err == http.ErrNoCookie {
 			// If the cookie is not set, return an unauthorized status
@@ -73,10 +78,12 @@ func Welcome(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
 	sessionToken := c.Value
 
 	// We then get the name of the user from our cache, where we set the session token
-	response, err := Connection.RetrieveCache(sessionToken)
+	response, err := base64.URLEncoding.DecodeString(sessionToken)
+
 	if err != nil {
 		// If there is an error fetching from cache, return an internal server error status
 		w.WriteHeader(http.StatusInternalServerError)
@@ -88,6 +95,7 @@ func Welcome(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
+
 	// Finally, return the welcome message to the user
-	w.Write([]byte(fmt.Sprintf("Welcome %s!", response)))
+	_, _ = w.Write([]byte(fmt.Sprintf("Welcome %s!", response)))
 }
