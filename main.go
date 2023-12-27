362a478da127a310a95f5bd6770e9b6923b04242
Teh|Striker <kristianhanus@gmail.com>
2023-12-27 12:04:39 +0100

    Viper configuration - Apply configurations

--- main.go
+++ main.go
@@ -15,26 +15,33 @@ import (
 
 func main() {
 	applicationConfiguration := config.ReadConfiguration()
+	applicationConfig := applicationConfiguration.Application
+	authorizationConfig := applicationConfiguration.Authorization
+	appRoute := mux.NewRouter().PathPrefix(applicationConfig.ContextPath).Subrouter()
 
-	coreRoute := mux.NewRouter()
-	appRoute := coreRoute.PathPrefix(applicationConfiguration.Application.ContextPath).Subrouter()
-
-	jwtConfig := jwt.NewConfigStruct()
 	userRepo := userRepository.NewLocalUserRepository()
 	userCache := userRepository.NewCacheConfig()
 	userService := userServices.NewLocalUserRepository(&userRepo, userCache)
-
 	userHandling := handlers.NewUserHandler(&userService)
-	jwtHandling := handlers.NewJwtHandler(&userService, jwtConfig)
-	cookiesHandling := handlers.NewCookiesHandler("session_token", &userService)
-
 	userHandling.EnrichRouter(appRoute)
-	jwtHandling.EnrichRouter(appRoute)
-	cookiesHandling.EnrichRouter(appRoute)
+
+	switch authorizationConfig.AuthorizationType {
+	case "jwt":
+		jwtConfig := jwt.NewConfigStruct()
+		jwtHandling := handlers.NewJwtHandler(&userService, jwtConfig)
+		jwtHandling.EnrichRouter(appRoute)
+		break
+	case "cookies":
+		cookiesHandling := handlers.NewCookiesHandler("session_token", &userService)
+		cookiesHandling.EnrichRouter(appRoute)
+		break
+	default:
+		log.Fatal("unrecognized authorization type")
+	}
 
 	corsHandler := cors.AllowAll().Handler(appRoute)
-	address := fmt.Sprintf(":%s", applicationConfiguration.Application.Port)
+	address := fmt.Sprintf(":%s", applicationConfig.Port)
 
-	log.Println("Listening on port:", applicationConfiguration.Application.Port)
+	log.Println("Listening on port:", applicationConfig.Port)
 	log.Println(http.ListenAndServe(address, corsHandler))
 }
