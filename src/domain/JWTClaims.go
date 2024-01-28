1d0e1543bb279af7fe76644c79afa666a9fff802
Teh|Striker <kristianhanus@gmail.com>
2024-01-28 14:03:41 +0100

    Code cleanup before PR

--- src/domain/JWTClaims.go
+++ src/domain/JWTClaims.go
@@ -4,6 +4,8 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
+// TODO Consider using only username
+
 type UserClaims struct {
 	User UserDTO
 	jwt.RegisteredClaims
--- src/domain/UserCredentials.go
+++ src/domain/UserCredentials.go
@@ -1,10 +1,5 @@
 package domain
 
-import (
-	"golang.org/x/crypto/bcrypt"
-	"log"
-)
-
 // Create a struct that models the structure of a user, both in the request body, and in the DB
 type UserCredentials struct {
 	Username string `json:"username"`
@@ -16,23 +11,4 @@ func NewCredentials(username, password string) UserCredentials {
 		Username: username,
 		Password: password,
 	}
-}
-
-func (c *UserCredentials) ClearPassword() {
-	c.Password = ""
-}
-
-func (c *UserCredentials) EncryptPassword() error {
-	encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(c.Password), bcrypt.DefaultCost)
-	if err != nil {
-		log.Println("password encryption error:", err)
-		return err
-	}
-
-	c.Password = string(encryptedPassword)
-	return nil
-}
-
-func (c *UserCredentials) ValidatePassword(inputPassword string) error {
-	return bcrypt.CompareHashAndPassword([]byte(c.Password), []byte(inputPassword))
 }
